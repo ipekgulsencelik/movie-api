372085c200103fee07f9fa5825dbf1a162232128
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-11 13:41:40 +0300

    using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import "./App.css";
 
@@ -9,12 +9,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch("https://swapi.dev/api/films/");
-
+      
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -34,7 +34,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies.</p>;
 
